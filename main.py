f93387512fd2560ffff3a962e056aef07d2ced19
edtcz <joxe736@gmail.com>
2022-01-20 19:23:28 -0800

    imports

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("PROBANDO OTRA VEZ")
+"""
+
+
+"""
+
+import discord
+from discord.ext import commands
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.command()
+async def hola(ctx):
+    await ctx.reply('Hola!')
+
+bot.run("")
+
